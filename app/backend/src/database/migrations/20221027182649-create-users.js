7faf69b81538b31d4e2145272602ff3712e35646
Fran Ribeiro <ribeirofrv@gmail.com>
2022-10-27 15:42:28 -0300

    feat(database): create users migrations

---
+++ app/backend/src/database/migrations/20221027182649-create-users.js
@@ -0,0 +1,38 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable(
+      "users",
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          autoIncrement: true,
+          allowNull: false,
+        },
+        username: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        role: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        email: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        password: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+      },
+      { timestamps: false }
+    );
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable("users");
+  },
+};
