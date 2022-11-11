9511cf89867de128e9d553baac6bae0687dab5ec
Fran Ribeiro <ribeirofrv@gmail.com>
2022-11-11 00:44:33 -0300

    feat(auth): add jwt authentication

---
+++ app/backend/src/auth/jwt.auth.ts
@@ -0,0 +1,29 @@
+import * as jwt from 'jsonwebtoken';
+import UnauthorizedError from '../errors/UnauthorizedError';
+
+export interface IPayload {
+  id: number;
+  username: string;
+  email: string;
+}
+
+export const SECRET_KEY: jwt.Secret = 'jwt_secret';
+export const CONFIG: jwt.SignOptions = {
+  algorithm: 'HS256',
+};
+
+export const generateToken = (user: IPayload): string => {
+  const { id, username, email } = user;
+  const token = jwt.sign(
+    { id, username, email },
+    SECRET_KEY,
+    CONFIG,
+  );
+  return token;
+};
+
+export const verifyToken = (token: string) => {
+  const result = jwt.verify(token, SECRET_KEY);
+  if (!result) throw new UnauthorizedError('Unauthorized');
+  return result;
+};
