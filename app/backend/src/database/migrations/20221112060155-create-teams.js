c5c16038976ad80c8eb9d7be4175dd29181a7013
Fran Ribeiro <ribeirofrv@gmail.com>
2022-11-12 03:26:23 -0300

    feat(database): add a migration and a model to the teams table

---
+++ app/backend/src/database/migrations/20221112060155-create-teams.js
@@ -0,0 +1,27 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable(
+      "teams",
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          autoIncrement: true,
+          allowNull: false,
+        },
+        teamName: {
+          type: Sequelize.STRING,
+          allowNull: false,
+          field: "team_name",
+        },
+      },
+      { timestamps: false }
+    );
+  },
+
+  async down(queryInterface, _Sequelize) {
+    await queryInterface.dropTable("teams");
+  },
+};
