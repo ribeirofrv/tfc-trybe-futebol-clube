83ed6b9e7e12eae8e0bfed57e3e08cfd23a5baa6
Fran Ribeiro <ribeirofrv@gmail.com>
2022-11-12 15:39:45 -0300

    refactor(controllers): remove comment

--- app/backend/src/controllers/teams.controller.ts
+++ app/backend/src/controllers/teams.controller.ts
@@ -10,8 +10,6 @@ export default class TeamsController {
 
   public async findAll(_request: Request, response: Response) {
     const allTeams = await this.teamsService.findAll();
-    console.log('All teams Controll: ', allTeams);
-
     response.status(200).json(allTeams);
   }
 }
