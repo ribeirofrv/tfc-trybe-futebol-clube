117a812737b9041e90c2bf9884f2c794ca402bfe
Fran Ribeiro <ribeirofrv@gmail.com>
2022-11-12 00:22:53 -0300

    feat(routes): add GET /login/validate route

--- app/backend/src/routes/UsersRoute.ts
+++ app/backend/src/routes/UsersRoute.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import UserService from '../services/user.services';
 import UserController from '../controllers/user.controller';
+import { IUserService } from '../interfaces/services/IUserService';
 
 const userService = new UserService();
-const userController = new UserController(userService);
+const userController = new UserController(userService as IUserService);
 const userRouter = Router();
 
 userRouter
-  .post('/login', (req, res) => userController.login(req, res));
+  .post('/login', (req, res) => userController.login(req, res))
+  .get('/login/validate', (req, res) => userController.validate(req, res));
 
 export default userRouter;
