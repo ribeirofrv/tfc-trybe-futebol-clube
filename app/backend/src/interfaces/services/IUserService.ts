47b6825b513ad53cca3dc276530d50c74bea830b
Fran Ribeiro <ribeirofrv@gmail.com>
2022-11-12 00:29:08 -0300

    feat(interfaces): adds decoded user interface for token verification

--- app/backend/src/interfaces/services/IUserService.ts
+++ app/backend/src/interfaces/services/IUserService.ts
@@ -2,4 +2,12 @@ import { ICredential } from '../IUser';
 
 export interface IUserService {
   login(login: ICredential): Promise<string>;
+  validate(token: string): Promise<string>;
+}
+
+export interface IDecodedUser {
+  id: number;
+  username: string;
+  email: string;
+  iat: number;
 }
