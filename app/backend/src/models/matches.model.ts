fd80e25a4cd10d2bccbabed4914849038213ff0c
Fran Ribeiro <ribeirofrv@gmail.com>
2022-11-13 02:13:29 -0300

    feat(models): add the findAll method to find matches

---
+++ app/backend/src/models/matches.model.ts
@@ -0,0 +1,35 @@
+import IMatchesModel from '../interfaces/models/IMatchesModel';
+import IMatchesDTO from '../interfaces/dtos/IMatchDTO';
+import Matches from '../database/models/MatchesModel';
+import Teams from '../database/models/TeamsModel';
+
+export default class MatchesModel implements IMatchesModel {
+  private _model: typeof Matches;
+
+  constructor() {
+    this._model = Matches;
+  }
+
+  async findAll(): Promise<IMatchesDTO[] | null> {
+    const matches = await this._model.findAll({
+      include: [
+        { model: Teams, as: 'teamHome', attributes: ['teamName'] },
+        { model: Teams, as: 'teamAway', attributes: ['teamName'] },
+      ],
+    });
+    return matches;
+  }
+
+  async findAllByProgress(status: boolean): Promise<IMatchesDTO[] | null> {
+    const matches = await this._model.findAll({
+      include: [
+        { model: Teams, as: 'teamHome', attributes: ['teamName'] },
+        { model: Teams, as: 'teamAway', attributes: ['teamName'] },
+      ],
+      where: {
+        inProgress: status,
+      },
+    });
+    return matches;
+  }
+}
