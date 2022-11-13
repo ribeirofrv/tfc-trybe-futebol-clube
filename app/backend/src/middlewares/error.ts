4cacd1af2986f5672d397923b27a9487d8b2f8c7
Fran Ribeiro <ribeirofrv@gmail.com>
2022-11-12 22:15:15 -0300

    refactor(middlewares): remove unnecessary conditional

--- app/backend/src/middlewares/error.ts
+++ app/backend/src/middlewares/error.ts
@@ -3,16 +3,12 @@ import { JsonWebTokenError } from 'jsonwebtoken';
 
 const ErrorMiddleware: ErrorRequestHandler = (error, _request, response, _next) => {
   const { status, message } = error;
-  console.log(':: ', error);
-  if (message) {
-    return response.status(status).json({ message });
-  }
-
+  // console.log(':: ', error);
   if (error instanceof JsonWebTokenError) {
-    return response.status(401).json({ message: 'Invalid token' });
+    return response.status(401).json({ message: 'Token must be a valid token' });
   }
 
-  return response.status(500).json({ message: 'Something went wrong!' });
+  return response.status(status || 500).json({ message });
 };
 
 export default ErrorMiddleware;
