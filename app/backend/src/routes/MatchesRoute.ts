bc67cac7a095794786d0eb42634ab4e8ad73a0ec
Fran Ribeiro <ribeirofrv@gmail.com>
2022-11-17 16:52:40 -0300

    feat(routes): add POST /matches route

--- app/backend/src/routes/MatchesRoute.ts
+++ app/backend/src/routes/MatchesRoute.ts
@@ -1,13 +1,16 @@
 import { Router } from 'express';
+import authentication from '../middlewares/authentication';
 import MatchesController from '../controllers/matches.controller';
 import MatchesService from '../services/matches.services';
-// import IMatchesService from '../interfaces/services/IMatchesService';
 
 const matchesService = new MatchesService();
 const matchesController = new MatchesController(matchesService);
 const matchesRouter = Router();
 
 matchesRouter
-  .get('/matches', (req, res) => matchesController.findAllMatches(req, res));
+  .get('/matches', (req, res) => matchesController.findAllMatches(req, res))
+  .post('/matches', authentication, (req, res) => {
+    matchesController.createMatch(req, res);
+  });
 
 export default matchesRouter;
